13835b6650409d6e9a810f85db0e2654d9250d86
srirakshyarisha-hub <srirakshyarisha@gmail.com>
2026-09-16 19:32:01 +0530

    Add files via upload

---
+++ urls[1].py
@@ -0,0 +1,10 @@
+"""expense_tracker URL Configuration"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('expenses.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+]
