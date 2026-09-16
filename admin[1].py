27fef0b9ad4cc0e47f3242ead7b490ff59644b58
srirakshyarisha-hub <srirakshyarisha@gmail.com>
2026-09-16 20:31:04 +0530

    Add files via upload

---
+++ admin[1].py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Expense
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'amount', 'category', 'payment_method', 'expense_date', 'created_at')
+    list_filter = ('category', 'payment_method', 'expense_date')
+    search_fields = ('title', 'description')
